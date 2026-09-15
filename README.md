eee6b4bc067bfc8996f7918b44c9bcebac46c9ae
cybervega88 <rvega@cyberm4ximus.com>
2026-09-15 14:22:44 -0600

    Initial CyberStay reservation API

---
+++ README.md
@@ -0,0 +1,42 @@
+# CyberStay Hotels - DevSecOps Lab
+
+CyberStay is a fictional hotel reservation platform used to learn
+AWS DevSecOps and Cloud Security.
+
+## Architecture
+
+Current AWS architecture:
+
+Client
+→ API Gateway
+→ Lambda
+→ DynamoDB
+
+## AWS Services
+
+- Amazon API Gateway
+- AWS Lambda
+- Amazon DynamoDB
+- AWS IAM
+- Amazon CloudWatch
+
+## Current API
+
+POST /reservations
+
+## Project Goal
+
+Build a secure DevSecOps pipeline for CyberStay Hotels.
+
+The project will progressively implement:
+
+- Source Control
+- CI/CD
+- Infrastructure as Code
+- SAST
+- SCA
+- Secret Scanning
+- IaC Security Scanning
+- Security Gates
+- AWS Deployment
+- Monitoring and Detection
---
+++ application/lambda_function.py
@@ -0,0 +1,30 @@
+import json
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("cyberstay-reservations")
+
+
+def lambda_handler(event, context):
+
+    body = json.loads(event["body"])
+
+    reservation = {
+        "reservationId": body["reservationId"],
+        "hotel": body["hotel"],
+        "room": body["room"],
+        "status": "CONFIRMED"
+    }
+
+    table.put_item(Item=reservation)
+
+    return {
+        "statusCode": 201,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "Reservation created successfully",
+            "reservation": reservation
+        })
+    }
